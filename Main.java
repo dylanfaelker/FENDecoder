faff3fe7c0aaf16c4773df0e17abc82cd5a37182
Dylan Faelker <84638978+dylpykill@users.noreply.github.com>
2021-07-31 21:12:53 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ class Main
   {
     Scanner keyboard = new Scanner(System.in);
 
-    //Gets the FEn number from the user
+    //Gets the FEN number from the user
     System.out.print("FEN number: ");
     String fen = keyboard.nextLine();
 
@@ -22,38 +22,52 @@ class Main
     int row = 0;
     int col = 0;
 
+    int numSpaces = 0;
+
     for(int i = 0; i < fen.length(); i++)
     {
       char ch = fen.charAt(i);
 
-      if(ch == '/')
+      //for when the FEN number is giving instructions about piece positions
+      if(numSpaces = 0)
       {
-        row++;
-        col = 0;
+        //for when the FEN number is indicating a new line
+        if(ch == '/')
+        {
+          row++;
+          col = 0;
+        }
+
+        //for when the FEN number is indicating a couple of squares without pieces
+        if(Character.isDigit(ch))
+        {
+          addEmpty(row, col, Character.getNumericValue(ch));
+          col += Character.getNumericValue(ch);
+          
+        }
+
+        //for when the FEN number is indicating a piece on a square
+        if(Character.isLetter(ch))
+        {
+          addPiece(row, col, ch);
+          col++;
+        }
       }
 
-      if(Character.isDigit(ch))
-      {
-        addEmpty(row, col, Character.getNumericValue(ch));
-        col += Character.getNumericValue(ch);
-        
-      }
-
-      if(Character.isLetter(ch))
-      {
-        addPiece(row, col, ch);
-        col++;
-      }
-
+      if(Character.isWhiteSpace(ch))
+        {
+          numSpaces++;
+        }
     }
   }
 
-  
+  //puts the piece into the board array
   private static void addPiece(int row, int col, char piece)
   {
     board[row][col] = piece; 
   }
 
+  //adds a blank space for squares without pieces. Fixes formatting.
   private static void addEmpty(int row, int col, int amt)
   {
     for(int i = 0; i < amt; i++)
@@ -62,6 +76,7 @@ class Main
     }
   }
 
+  //prints a image of the board to the console with info of whos turn it is and castling rights and enpassent possiblites
   private static void printBoard()
   {
     for(int r = 0; r < 8; r++)
